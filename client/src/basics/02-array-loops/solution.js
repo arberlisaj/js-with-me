efd5e1c4f72dd5ccc66fca84b4da79e6623f3acd
arberlisaj <the.arberlisaj@gmail.com>
2024-11-18 21:04:35 +0100

    refactor: chore: Move all documentation to docs directory

--- client/src/basics/02-array-loops/solution.js
+++
@@ -1,7 +0,0 @@
-let numbersArr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
-
-for (let i = 2; i < numbersArr.length; i += 3) {
-  console.log(numbersArr[i]);
-}
